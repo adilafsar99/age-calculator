c4457def5de6190209b65099ee7915ab320c84f7
Muhammad Adil <adilafsar99@gmail.com>
2021-06-01 18:48:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ var currentYear = Math.floor(currentMillis / (1000 * 60 * 60 * 24 * 30 * 12)) + 
 var birthYear = "";
 var validBirthYear = false;
 while (birthYear === "" || birthYear === null || validBirthYear === false) {
-				birthYear = prompt("Enter your birth year:");
-				if (birthYear === null) {
+	birthYear = prompt("Enter your birth year:");
+		if (birthYear === null) {
 								alert("Please proceed!");
-				}
-				else if (birthYear === "") {
+		}
+		else if (birthYear === "") {
 								alert("You entered nothing!");
-				}
-				else if (birthYear != Number(birthYear)) {
+		}
+		else if (birthYear != Number(birthYear)) {
 								alert("This isn't a number!");
-				}
-				else if (birthYear.indexOf(".") !== -1) {
+		}
+		else if (birthYear.indexOf(".") !== -1) {
 								alert("Year must be an integer!")
-				}
-				else if (birthYear.length !== 4) {
+		}
+		else if (birthYear.length !== 4) {
 								alert("Input should contain 4 digits!");
-				}
-				else if (birthYear > currentYear) {
+		}
+		else if (birthYear > currentYear) {
 								alert("Given year has yet to come!");
-				}
-				else if (birthYear < 0) {
+		}
+		else if (birthYear < 0) {
 								alert("Negative year is not possible!")
-				}
-				else {
+		}
+		else {
 								validBirthYear = true;
-				}
+		}
 }
 
 var age = currentYear - birthYear;
@@ -38,10 +38,10 @@ document.write("<p><span class='sub-heading'>Your Birth Year: </span>" + birthYe
 document.write("<p><span class='sub-heading'>Current Year: </span>" + currentYear + "</p>");
 document.write("<h2>Result</h2>");
 if (currentYear == birthYear) {
-				document.write("<p>You are less than a year old.</p>")
+   document.write("<p>You are less than a year old.</p>")
 }
 else if (birthYear < 1870) {
-				document.write("<p>You are WAY too old.</p>")
+   document.write("<p>You are WAY too old.</p>")
 }
 else {
    document.write("<p>You've been on Earth for " + age + " years.</p>");
